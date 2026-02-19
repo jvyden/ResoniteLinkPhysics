ad1d4b8aa10b8701b6c051097e141362926165eb
jvyden <jvyden@jvyden.xyz>
2026-02-19 15:25:09 -0500

    Cleanup, different size balls

--- ResoniteLinkPhysics/Ball.cs
+++ ResoniteLinkPhysics/Ball.cs
@@ -14,10 +14,13 @@ public class Ball
     {
         RigidPose pose = sim.Bodies[this.BodyHandle].Pose;
 
-        Slot slot = new();
-        slot.ID = this.SlotId;
-        slot.Position = new Field_float3 {Value = Unsafe.BitCast<Vector3, float3>(pose.Position)};
-        slot.Rotation = new Field_floatQ {Value = Unsafe.BitCast<Quaternion, floatQ>(pose.Orientation)};
+        Slot slot = new()
+        {
+            ID = this.SlotId,
+            Position = new Field_float3 {Value = Unsafe.BitCast<Vector3, float3>(pose.Position)},
+            Rotation = new Field_floatQ {Value = Unsafe.BitCast<Quaternion, floatQ>(pose.Orientation)}
+        };
+
         return new UpdateSlot
         {
             Data = slot,
